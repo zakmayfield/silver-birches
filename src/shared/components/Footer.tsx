f536ee78823f2f5c9645b0d65920e3d21b64ef48
Zak Mayfield <zakmayfield@gmail.com>
2024-05-08 10:51:59 -0400

    chore: footer styles

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -1,23 +1,32 @@
 import { FC } from 'react';
+import { Logo } from './Logo';
 
 interface FooterProps {}
 
 export const Footer: FC<FooterProps> = ({}) => {
   return (
-    <footer className='flex items-start justify-center gap-12 border'>
-      <div>
-        <h6>Contact</h6>
+    <footer className='flex items-center justify-center gap-12 border rounded-lg'>
+      <Logo />
 
-        <div>Content A</div>
-        <div>Content B</div>
-        <div>Content C</div>
-      </div>
+      <div className='flex items-start justify-center gap-12 border rounded-lg w-full'>
+        <div>
+          <h6>Contact</h6>
 
-      <div>
-        <h6>Socials</h6>
+          <div className='flex gap-6'>
+            <div>Content A</div>
+            <div>Content B</div>
+            <div>Content C</div>
+          </div>
+        </div>
 
-        <div>Content 1</div>
-        <div>Content 2</div>
+        <div>
+          <h6>Socials</h6>
+
+          <div className='flex gap-6'>
+            <div>Content 1</div>
+            <div>Content 2</div>
+          </div>
+        </div>
       </div>
     </footer>
   );
