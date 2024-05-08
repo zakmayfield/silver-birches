d0c77e8f7ee1df212205d112ff638747cbaa6311
Zak Mayfield <zakmayfield@gmail.com>
2024-05-08 10:38:51 -0400

    chore: nav links

--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -1,7 +1,13 @@
+import Link from 'next/link';
 import { FC } from 'react';
 
 interface NavbarProps {}
 
 export const Navbar: FC<NavbarProps> = ({}) => {
-  return <nav>Navbar</nav>;
+  return (
+    <nav className='flex gap-6'>
+      <Link href='/'>home</Link>
+      <Link href='/rates'>rates</Link>
+    </nav>
+  );
 };
