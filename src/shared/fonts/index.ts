5ed4ee560d536b3e76633fa2e1284e5be1b1e2c4
Zak Mayfield <zakmayfield@gmail.com>
2024-05-08 10:14:04 -0400

    chore: fonts

---
+++ src/shared/fonts/index.ts
@@ -0,0 +1,5 @@
+import { Inter, Quicksand } from 'next/font/google';
+const inter = Inter({ subsets: ['latin'] });
+const quicksand = Quicksand({ subsets: ['latin'] });
+
+export { inter, quicksand };
