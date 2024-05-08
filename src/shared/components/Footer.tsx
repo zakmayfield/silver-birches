b39e721c42c3c8e41560df93ab190d1af98693c4
Zak Mayfield <zakmayfield@gmail.com>
2024-05-08 10:46:01 -0400

    chore: footer boilerplate

--- src/shared/components/Footer.tsx
+++ src/shared/components/Footer.tsx
@@ -3,5 +3,22 @@ import { FC } from 'react';
 interface FooterProps {}
 
 export const Footer: FC<FooterProps> = ({}) => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className='flex items-start justify-center gap-12 border'>
+      <div>
+        <h6>Contact</h6>
+
+        <div>Content A</div>
+        <div>Content B</div>
+        <div>Content C</div>
+      </div>
+
+      <div>
+        <h6>Socials</h6>
+
+        <div>Content 1</div>
+        <div>Content 2</div>
+      </div>
+    </footer>
+  );
 };
